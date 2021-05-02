141017a8078f2580c916fcab57ea637bbc62c61f
sjkchang <sjkchang@gmail.com>
2021-05-01 20:39:56 -0700

    Setup landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import '../styles/App.css';
+import '../styles/Home.css'
 import image from './image.png';
 
 function Home() {
   return (
-    <div id="big" className="Home">
-      <h1>Steven Chang</h1>
-      <p>I like to make stuff</p>
-      <span><a href="https://github.com/sjkchang" target="_blank" rel="noreferrer"><i className='fa fa-2x fa-github-square'></i></a></span>
-      <span><a href="https://www.linkedin.com/in/steven-chang-7723b518b/" target="_blank" rel="noreferrer"><i className='fab fa-2x fa-linkedin'></i></a></span>
-      <img src={image}/>
+    <div>
+      <div className="title-container">
+        <h1 className="title">Steven Chang</h1>
+        <h4 className="sub-title">I like to make stuff</h4>
+        <span className="icon"><a href="https://github.com/sjkchang" target="_blank" rel="noreferrer"><i className='fa fa-2x fa-github-square'></i></a></span>
+        <span className="icon"><a href="https://www.linkedin.com/in/steven-chang-7723b518b/" target="_blank" rel="noreferrer"><i className='fab fa-2x fa-linkedin'></i></a></span>
+      </div>
+      <div className="image-container center">
+        <img src={image}/>
+      </div>
     </div>
   );
 }
