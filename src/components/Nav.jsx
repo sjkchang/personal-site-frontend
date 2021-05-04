45aaa0377371d1840a3ee7101d7b38272c02df3c
sjkchang <sjkchang@gmail.com>
2021-05-03 18:16:42 -0700

    Added dropshadow to nav bar when scrolling, removed redundant css from navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,35 @@
-import React, {state} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../styles/Nav.css';
 import {Link} from 'react-router-dom';
 import 'react-bootstrap';
 
 function Nav() {
+  const [scrolled, setScroll] = useState(false);
   
+  function handleScroll() {
+    const position = window.scrollY;
+    if (position > 1){
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+
+    }
+  });
+
+
   return (
-    <nav id="navbar" class="floating-nav"> 
+    <nav id="navbar" class={scrolled ? "floating-nav" : ""}> 
       <div className="navbar-wrapper">
         <div className='logo'>
-          <li><Link to='/'><i className='fas fa-lg fa-strikethrough'></i></Link></li>
+          <Link to='/'><i className='fas fa-lg fa-strikethrough'></i></Link>
         </div>
         <ul id="nav-list">
           <li className='nav-item'>
@@ -19,13 +39,10 @@ function Nav() {
             <Link to='/portfolio' className='nav-link'>Portfolio</Link>
           </li>
           <li className='nav-item'>
-            <Link to='/about' className='nav-link'>Resume</Link>
+            <Link to='/blog' className='nav-link'>Blog</Link>
           </li>
           <li className='nav-item'>
             <Link to='/contact-me' className='nav-link'>Contact Me</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/blog' className='nav-link'>Blog</Link>
           </li>
         </ul>
       </div>
